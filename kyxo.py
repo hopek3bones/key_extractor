8debe60e1d4353bd70a6f26ad8196ce251081bd3
Dr Mokira <arnoldmokira3d48@gamil.com>
2022-08-17 10:58:13 -0800

    README is updated.

--- kyxo.py
+++ kyxo.py
@@ -6,7 +6,7 @@ FR_MODEL = ('fr_core_news_md');
 EN_ALPHA = "0123456789abcdefghijklmnopqrstuvwxyz-'";
 FR_ALPHA = "0123456789abcdefghijklmnopqrstuvwxyz-éù'èçàîêâïôüûë";
 EXTRACT_PATTERN = None;
-SBT_AVR  = 1.5;
+SBT_AVR  = 3.0;
 NLP      = None;
 MTC      = None;
 ALP      = None;
@@ -146,9 +146,44 @@ def get_keywords(text):
 
 if __name__ == '__main__':
     eg = """
-Un réseau de télévision est un réseau de télécommunications destiné à la distribution de programmes télévisés. Cependant, le terme désigne désormais un groupement d'affiliés régionaux autour d'une chaîne de télévision centrale, offrant une programmation à plusieurs stations de télévision locales.
 
-Jusqu'au milieu des années 1980, la programmation télévisée de la plupart des pays du monde a été dominée par un petit nombre de réseaux de diffusion. Bon nombre des premiers réseaux de télévision (comme la BBC, NBC ou CBS) se sont développés à partir de réseaux de radio existants. 
+
+L'intelligence artificielle (IA) est un processus d'imitation de l'intelligence humaine qui repose sur la création et l'application d'algorithmes exécutés dans un environnement informatique dynamique. Son but est de permettre à des ordinateurs de penser et d'agir comme des êtres humains.
+
+Pour y parvenir, trois composants sont nécessaires :
+
+    Des systèmes informatiques
+    Des données avec des systèmes de gestion
+    Des algorithmes d'IA avancés (code)
+
+Pour se rapprocher le plus possible du comportement humain, l'intelligence artificielle a besoin d'une quantité de données et d'une capacité de traitement élevées.
+Quelles sont les origines de l'intelligence artificielle ?
+
+Depuis au moins le premier siècle avant notre ère, l'Homme s'est penché sur la création de machines capables d'imiter le raisonnement humain. Le terme « intelligence artificielle » a été créé plus récemment, en 1955 par John McCarthy. En 1956, John McCarthy et ses collaborateurs ont organisé une conférence intitulée « Dartmouth Summer Research Project on Artificial Intelligence » qui a donné naissance au machine learning, au deep learning, aux analyses prédictives et, depuis peu, aux analyses prescriptives. Un nouveau domaine d'étude est également apparu : la science des données.
+Pourquoi l'intelligence artificielle est-elle importante ?
+
+De nos jours, êtres humains et machines génèrent des données plus vite qu'il n'est humainement possible de les absorber et de les interpréter pour prendre des décisions complexes. L'intelligence artificielle est la base de tout apprentissage par un ordinateur et représente l'avenir des processus décisionnels complexes. Par exemple, la plupart des êtres humains peuvent apprendre à ne pas perdre à une simple partie de morpion, alors qu'il existe 255 168 actions possibles, dont 46 080 mènent à un match nul. En revanche, les champions du jeu de dames sont plus rares, étant donné qu'il existe plus de 500 x 1018 (500 trillions) de coups possibles. Les ordinateurs sont capables de calculer ces combinaisons et les meilleures permutations possibles très efficacement, afin de prendre la bonne décision. L'IA (avec son évolution logique, le machine learning) et le deep learning représentent l'avenir de la prise de décisions.
+Utilisations de l'intelligence artificielle
+
+L'IA est présente dans notre quotidien. Elle est par exemple utilisée par les services de détection des fraudes des établissements financiers, pour la prévision des intentions d'achat et dans les interactions avec les services clients en ligne. Voici quelques exemples : 
+
+    Détection des fraudes. Dans le secteur de la finance, l'intelligence artificielle est utilisée de deux manières. Les applications qui notent les demandes de crédit utilisent l'IA pour évaluer la solvabilité des consommateurs. Des moteurs d'IA plus avancés sont chargés de surveiller et de détecter en temps réel les paiements frauduleux réalisés par carte bancaire.
+    Service client virtuel (SCV). Les centres d'appel utilisent un SCV pour prédire les demandes de leurs clients et y répondre sans intervention humaine. La reconnaissance vocale et un simulateur de dialogue humain constituent le premier point d'interaction avec le service client. Les demandes plus complexes requièrent quant à elles une intervention humaine.
+    Lorsqu'un internaute ouvre une fenêtre de dialogue sur une page web (chatbot), son interlocuteur est souvent un ordinateur exécutant une forme d'IA spécialisée. Si le chatbot ne parvient pas à interpréter la question ou à résoudre le problème, un agent humain prend le relais. Ces échecs d'interprétation sont envoyés au système de machine learning afin d'améliorer les futures interactions de l'application d'IA.
+
+NetApp et l'intelligence artificielle
+
+En tant que référence en matière de gestion de données dans le cloud hybride, NetApp comprend l'importance de l'accès aux données, de leur gestion et de leur contrôle. NetApp® Data Fabric fournit un environnement de gestion unifiée des données qui couvre les terminaux, les data centers et plusieurs clouds hyperscale. Il permet aux entreprises, quelle que soit leur taille, d'accélérer les applications stratégiques, d'améliorer la visibilité sur les données, d'en optimiser la protection et d'améliorer l'agilité fonctionnelle.
+
+Les solutions d'IA de NetApp reposent sur des composants de base clés : 
+
+    Le logiciel ONTAP® permet d'exploiter l'IA et le deep learning localement et dans le cloud hybride.
+    Les systèmes FAS 100 % Flash accélèrent les workloads d'IA et de deep learning, tout en éliminant les goulots d'étranglement qui affectent les performances.
+    Le logiciel ONTAP Select permet de collecter les données efficacement à la périphérie à l'aide de terminaux IoT et de points d'agrégation.
+    Cloud Volumes peut être utilisé pour créer rapidement des prototypes pour de nouveaux projets. Il permet de recevoir et d'envoyer des données d'IA depuis et vers le cloud.
+
+NetApp a également commencé à intégrer l'analytique Big Data et l'intelligence artificielle dans ses propres produits et services, notamment avec Active IQ®, qui utilise des milliards de points de données, l'analyse prédictive et un moteur puissant de machine learning afin de proposer des recommandations proactives de support client pour les environnements IT complexes. Active IQ est une application de cloud hybride conçue à l'aide des mêmes produits et technologies NetApp que nos clients utilisent pour créer leurs solutions d'IA dans plusieurs domaines.
+
 """
     init(FR_MODEL, FR_ALPHA);
     ks = get_keywords(eg);
